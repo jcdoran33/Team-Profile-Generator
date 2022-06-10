6005c28cebb9cb647b4e4854bebcd95bb0593934
Jack Doran <jcdoran33@gmail.com>
2022-06-10 17:15:47 -0400

    added structure into emplyee.js file. started on employee.test.js

---
+++ test/Employee.test.js
@@ -0,0 +1,8 @@
+// require the lib -> Employee.js file
+const { default: TestRunner } = require("jest-runner");
+const Employee = require("../lib/Employee");
+
+//run tests....
+test("", () => {
+
+});
