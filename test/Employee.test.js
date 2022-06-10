ceb94c48b5492e15140c81a5ed89f0c1586f2217
Jack Doran <jcdoran33@gmail.com>
2022-06-10 17:36:04 -0400

    work on employee.test.js

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,8 +1,11 @@
 // require the lib -> Employee.js file
+const { exportAllDeclaration } = require("@babel/types");
 const { default: TestRunner } = require("jest-runner");
 const Employee = require("../lib/Employee");
 
 //run tests....
-test("", () => {
-
+test("Initialization of Employee class", () => {
+    const obj = new Employee(id, name, email, role);
+    //how do we test to see that a class anme Employee was created?
+    expect(obj).toEqual(Employee(id, name, email, role));
 });
