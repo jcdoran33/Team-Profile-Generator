cc292340313ee1e4dac29dd067cf31357e12dd1e
Jack Doran <jcdoran33@gmail.com>
2022-06-12 14:31:19 -0400

    completed first 4 test for Employee class

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -6,10 +6,22 @@ const Employee = require("../lib/Employee");
 test("Initialization of Employee class", () => {
     const obj = new Employee(7, "Paul", "paul@email.net");
     //how do we test to see that a class anme Employee was created?
-    expect(obj).toEqual(new Employee(7, "Paul", "paul@email.net"));
+    // expect(obj).toEqual(new Employee(7, "Paul", "paul@email.net"));
+    expect(obj instanceof Employee).toEqual(true);
 });
 
 test("Constructor initialized with a name", () => {
     const obj = new Employee(5, "Steve", "email@email.com");
     expect(obj.name).toEqual("Steve");
 });
+
+test("Constructor created with an ID", () => {
+    const obj = new Employee(5, "Steve", "email@email.com");
+    expect(obj.id).toBeTruthy();
+});
+
+test("Constructor initiated with an email", () => {
+    const obj = new Employee(5, "Steve", "email@email.com");
+    expect(obj.email).toBeTruthy();
+});
+
