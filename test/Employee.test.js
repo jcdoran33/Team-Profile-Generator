6e98c919e492aa11e561fc21632487679680207d
Jack Doran <jcdoran33@gmail.com>
2022-06-12 13:21:01 -0400

    removed role parameter from employee object

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -4,7 +4,12 @@ const Employee = require("../lib/Employee");
 
 //run tests....
 test("Initialization of Employee class", () => {
-    const obj = new Employee(id, name, email, role);
+    const obj = new Employee(id, name, email);
     //how do we test to see that a class anme Employee was created?
-    expect(obj).toEqual(Employee(id, name, email, role));
+    expect(obj).toEqual(Employee(id, name, email));
+});
+
+test("Constructor initialized with a name", () => {
+    const obj = new Employee(5, "Steve", "email@email.com");
+    expect(obj.name).toEqual("Steve");
 });
