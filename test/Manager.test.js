ca84a001553228daa1bdb160f4ee477d1afad18a
Jack Doran <jcdoran33@gmail.com>
2022-06-12 15:21:25 -0400

    added getROle and getOfficeNum to manager.js. Also created first test case in manager.test.js

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,3 +1,8 @@
 //require - the Employee.js file AND the Manager.js file
 const Employee = require("../lib/Employee");
 const Manager = require("../lib/Manager");
+
+test ("Constructor should set an office number", () => {
+    const obj = new Manager(1, "Mike", "email@email.com", 5)
+    expect(obj.officeNum).toEqual(5);
+});
