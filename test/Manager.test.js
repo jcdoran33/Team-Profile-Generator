79e2be4ff7d7cde96d91ecc08066cd73b79e17f1
Jack Doran <jcdoran33@gmail.com>
2022-06-12 15:24:39 -0400

    added test case for getRole in the Manager test file

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -2,7 +2,12 @@
 const Employee = require("../lib/Employee");
 const Manager = require("../lib/Manager");
 
-test ("Constructor should set an office number", () => {
+test("Constructor should set an office number", () => {
     const obj = new Manager(1, "Mike", "email@email.com", 5)
     expect(obj.officeNum).toEqual(5);
+});
+
+test("Method getRole should always return Manager", () => {
+    const obj = new Manager();
+    expect(obj.getRole()).toEqual("Manager");
 });
